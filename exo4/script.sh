7404e0a38d9ae517bb8875847ba109e219d7dc7e
AlexandreHoumeau <alexandre.houmeau@gmail.com>
2020-10-09 11:03:52 +0200

    fix: exo 4

--- exo4/script.sh
+++ exo4/script.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-cut -d: -f1 /etc/group | while IFS= read -r line
-do
-  if [ $1 = $line ] 
-  then 
-    echo "$1 est dans le fichier etc/group"
+FOUND="F"
+for n in $(cat /etc/group )
+do  
+  tmp=$( echo $n | cut -d: -f1)
+    if [ $1 = $tmp ]
+    then
+      FOUND="T"
     fi
 done
+
+if [ $FOUND = "T" ]
+then
+  echo "$1 présent dans /etc/group"
+else
+  echo "$1 n'est pas dans /etc/group"
+fi
+  
