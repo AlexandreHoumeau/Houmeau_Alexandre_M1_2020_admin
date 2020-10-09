639e1536461bab6698c0e65219cd54cf3424d10e
AlexandreHoumeau <alexandre.houmeau@gmail.com>
2020-10-09 11:00:19 +0200

    feat: new Readme.md and fix exo3

--- exo3/script.sh
+++ exo3/script.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-cut -d: -f1 /etc/passwd | while IFS= read -r line
-do
-  if [ $1 = $line ] 
-  then 
-    echo "$1 est dans le fichier etc/passwd"
+FOUND="F"
+for n in $(cat /etc/passwd )
+do  
+  tmp=$( echo $n | cut -d: -f1)
+    if [ $1 = $tmp ]
+    then
+      FOUND="T"
     fi
 done
+
+if [ $FOUND = "T" ]
+then
+  echo "$1 présent dans /etc/passwd"
+else
+  echo "$1 n'est pas dans /etc/passwd"
+fi
+  
